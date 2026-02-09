265a983da4acabae1d1446a74042962833c6e3e7
Leo Mav <13514096+leonmavr@users.noreply.github.com>
2026-02-09 13:20:09 +0100

    copy formatted output to variable and to clipboard

--- lua/mathfloat/init.lua
+++ lua/mathfloat/init.lua
@@ -3,6 +3,35 @@
 local selection = require("mathfloat.selection")
 local eval      = require("mathfloat.eval")
 local ui        = require("mathfloat.ui")
+local matrix_fn = require("mathfloat.functions.matrix")
+
+-- copy scalar to variable
+local function fmt_number(x)
+  if type(x) ~= "number" then
+    return tostring(x)
+  end
+  return string.format("%.6g", x)
+end
+
+local function to_bmatrix(m)
+  local rows = {}
+  for i = 1, m.r do
+    local cols = {}
+    for j = 1, m.c do
+      cols[#cols + 1] = fmt_number(m._data[i][j])
+    end
+    rows[#rows + 1] = table.concat(cols, " & ")
+  end
+  return "\\begin{bmatrix} " .. table.concat(rows, " \\\\ ") .. " \\end{bmatrix}"
+end
+
+-- copy matrix to variable as bmatrix format
+local function format_for_global(result)
+  if matrix_fn.is_matrix(result) then
+    return to_bmatrix(result)
+  end
+  return tostring(result)
+end
 
 -- public API
 local M = {}
@@ -17,6 +46,14 @@ function M.calculate_selection()
     vim.notify("Texeval error: " .. err, vim.log.levels.ERROR)
     return
   end
+
+  -- store Texeval's output to a global variable:
+  -- - Vimscript: :echo g:texeval_result
+  -- - Lua:       print(_G.texeval_result)
+  local formatted = format_for_global(result)
+  vim.g.texeval_result = formatted
+  _G.texeval_result = formatted
+
   ui.show(result)
 end
 
